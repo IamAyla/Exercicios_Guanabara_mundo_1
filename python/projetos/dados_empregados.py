c32ebb7b962b3071486150f5ada7668124bc8383
Ayla Morais <107047181+IamAyla@users.noreply.github.com>
2023-08-29 20:02:55 -0300

    Create dados_empregados.py

---
+++ python/projetos/dados_empregados.py
@@ -0,0 +1,17 @@
+class Employee:
+  def __init__(self, name, job):
+    self.name = name
+    self.job = job
+  
+  def tasks(self):
+    def __init__(self, name, job, staff):
+      super().__init__(name, job)
+      self.staff = staff
+
+    def tasks(self):
+      print('Oversees: ')
+      print(staff)
+
+class Associate(Employee):
+  def tasks(self):
+    print('Take orders from manager')
